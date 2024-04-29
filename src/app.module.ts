bf42eeb051321b3447bac082a9a5df2b6f813134
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2024-04-29 23:06:01 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CategoryModule } from './category/category.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, AuthModule, CategoryModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
